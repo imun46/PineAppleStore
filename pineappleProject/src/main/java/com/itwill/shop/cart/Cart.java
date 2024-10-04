05754dd7bf78185207f007d9ddf248886e25232d
ldh9709 <zszz5434@gmail.com>
2024-10-04 19:44:05 +0900

    DTO TEST

--- pineappleProject/src/main/java/com/itwill/shop/cart/Cart.java
+++ pineappleProject/src/main/java/com/itwill/shop/cart/Cart.java
@@ -1,5 +1,11 @@
 package com.itwill.shop.cart;
 
-public class Cart {
+import com.itwill.shop.customer.Customer;
 
+public class Cart {
+	
+	private Integer cart_no;//PK
+	private Integer cart_qty;
+	private Customer customer_no;//FK
+	
 }
--- pineappleProject/src/main/java/com/itwill/shop/customer/CustomerCoupons.java
+++ pineappleProject/src/main/java/com/itwill/shop/customer/CustomerCoupons.java
@@ -1,5 +1,26 @@
 package com.itwill.shop.customer;
 
-public class CustomerCoupons {
+import java.util.Date;
 
+import com.itwill.shop.coupon.Coupon;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class CustomerCoupons {
+	
+	private Integer customer_coupons_no;//PK
+	private Date customer_coupons_enddate;
+	private String customer_coupons_status;
+	
+	private Coupon coupon_no;//FK
+	private Customer customer_no;//FK
+	
 }
