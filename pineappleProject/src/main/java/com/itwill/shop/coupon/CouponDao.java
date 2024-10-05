37b22f495349f04b8add5c3bce9092e7a4b253f9
Dohyun <Dohyun@DESKTOP-3EPP0DH>
2024-10-05 16:08:36 +0900

    Customer Coupon 작업

--- pineappleProject/src/main/java/com/itwill/shop/coupon/CouponDao.java
+++ pineappleProject/src/main/java/com/itwill/shop/coupon/CouponDao.java
@@ -1,5 +1,18 @@
 package com.itwill.shop.coupon;
 
-public interface CouponDao {
 
+public interface CouponDao {
+	
+	//쿠폰 정보 추가(어드민용 쿠폰 발급)
+	int insert(Coupon coupon) throws Exception;
+	
+	//쿠폰 사용 여부(쿠폰 사용시 사용불가 혹은 완료 상태로 변경)
+	int update(Coupon coupon) throws Exception;
+	
+	//쿠폰 삭제(보류)
+	int delete(Integer couponNo) throws Exception;
+	
+	//쿠폰 조회(사용자 번호로 쿠폰 번호 조회)
+	Coupon findCoupon(Integer customerNo) throws Exception;
+	
 }
--- pineappleProject/src/main/java/com/itwill/shop/coupon/CouponDaoImpl.java
+++ pineappleProject/src/main/java/com/itwill/shop/coupon/CouponDaoImpl.java
@@ -1,5 +1,46 @@
 package com.itwill.shop.coupon;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.itwill.shop.mapper.CouponMapper;
+import com.itwill.shop.mapper.CustomerCouponsMapper;
+
 public class CouponDaoImpl implements CouponDao {
+	
+	private  SqlSessionFactory sqlSessionFactory;
+	
+	public CouponDaoImpl() throws Exception {
+		this.sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(Resources.getResourceAsStream("mybatis-config-mapper-interface.xml"));
+	}
+	
+					/*** 쿠폰 발급 메소드 (어드민용)***/
+	@Override
+	public int insert(Coupon coupon) throws Exception {
+		System.out.println("CouponDaoImpl : insert 메소드 호출");
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		return 0;
+	}
+	
+	@Override
+	public int update(Coupon coupon) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Integer couponNo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Coupon findCoupon(Integer customerNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
