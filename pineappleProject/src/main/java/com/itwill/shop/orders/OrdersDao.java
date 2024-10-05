7f823a1f43f52e92f98ddce59b69191bc300aad5
imjeong <imjeong@jeong>
2024-10-05 17:28:07 +0900

    Order Dao 수정

--- pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDao.java
+++ pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDao.java
@@ -1,5 +1,19 @@
 package com.itwill.shop.orders;
 
-public interface OrdersDao {
+import java.util.List;
 
+public interface OrdersDao {
+	//주문 등록
+	int insert(Orders order) throws Exception;
+
+	
+	//주문 전체 리스트(특정 사용자)
+	List<Orders> selectAll() throws Exception;
+	
+	//주문+주문 아이템(특정사용자, 특정 주문)
+	List<Orders> selectOrderByOrderItems() throws Exception;
+	
+	// 주문 1건 삭제(특정 주문)
+	int delete(int orderNo) throws Exception;
+	
 }
