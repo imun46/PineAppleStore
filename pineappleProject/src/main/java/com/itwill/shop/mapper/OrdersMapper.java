c8f9eddb9c3a4b945277d11617c2b8cfa0b5eee0
imjeong <imjeong@jeong>
2024-10-05 17:28:07 +0900

    Order Dao 수정

--- pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
+++ pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
@@ -1,5 +1,17 @@
 package com.itwill.shop.mapper;
 
-public class OrdersMapper {
+import java.util.List;
 
+import com.itwill.shop.orders.Orders;
+
+public interface OrdersMapper {
+	//주문 등록
+	int insert(Orders order) throws Exception;
+	
+	//주문 전체 리스트(특정 사용자)
+	List<Orders> selectAll() throws Exception;
+	
+	//주문+주문 아이템(특정사용자, 특정 주문)
+	
+	//주문 1개 
 }
--- pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDaoImpl.java
+++ pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDaoImpl.java
@@ -1,5 +1,41 @@
 package com.itwill.shop.orders;
 
-public class OrdersDaoImpl implements OrdersDao {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class OrdersDaoImpl implements OrdersDao {
+	SqlSessionFactory sqlSessionFactory;
+	public OrdersDaoImpl() throws Exception {
+		InputStream mybatisInputStream = Resources.getResourceAsStream("mybatis-config-mapper-interface");
+		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+		sqlSessionFactory = sqlSessionFactoryBuilder.build(mybatisInputStream);
+	}
+	@Override
+	public int insert(Orders order) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public List<Orders> selectAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Orders> selectOrderByOrderItems() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int delete(int orderNo) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 }
