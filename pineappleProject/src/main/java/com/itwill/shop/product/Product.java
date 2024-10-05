2c72744993d89cf0ab42fb3630120cb9ebb1443c
hannuripark <hannuripark@localhost>
2024-10-05 15:28:38 +0900

    product DTO 1차 수정 commit

--- pineappleProject/src/main/java/com/itwill/shop/product/Product.java
+++ pineappleProject/src/main/java/com/itwill/shop/product/Product.java
@@ -26,7 +26,13 @@ public class Product {
 	private String productDesc;			// 제품 설명
 	private String productStatus;		// 제품 상태 (품절,미품절)
 	
+<<<<<<< Upstream, based on origin/Ateam/기현
 	/******** FK *****************/
 	private List<Product> productList;  // 제품 전체 리스트
 	
+=======
+	private List<ProductImage> productImageList;	// 제품 이미지 리스트
+	private List<Review> productReviewList;			// 제품 리뷰 리스트
+	private List<ProductOption> productOptionList;	// 제품 옵션 리스트
+>>>>>>> 3e1ca06 product DTO 1차 수정 commit
 }
