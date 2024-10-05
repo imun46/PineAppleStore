ad353f381a5ebe91bb1b8a33aee321cfb3c007b1
imjeong <imjeong@jeong>
2024-10-05 22:07:34 +0900

    orders delete 완료

--- pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
+++ pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
@@ -10,10 +10,17 @@ public interface OrdersMapper {
 	int insertOrder(Orders order) throws Exception;
 	int insertOrderItem(OrdersItems orderItems) throws Exception;
 	
-	//주문 전체 리스트(특정 사용자)
+	//주문 전체 리스트(전체) 어드민
 	List<Orders> selectAll() throws Exception;
 	
+	//주문 전체 리스트(특정 사용자)
+	List<Orders> selectByOrderNo(int orderNo) throws Exception;
+	
 	//주문+주문 아이템(특정사용자, 특정 주문)
+<<<<<<< Upstream, based on origin/Ateam/기현
+=======
+	List<Orders> selectOrderByOrderItems(int customerNo, int orderNo) throws Exception;
+>>>>>>> bf33bf1 orders delete 완료
 	
 	//주문 1개 
 }
--- pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDaoImpl.java
+++ pineappleProject/src/main/java/com/itwill/shop/orders/OrdersDaoImpl.java
@@ -18,6 +18,7 @@ public class OrdersDaoImpl implements OrdersDao {
 		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
 		sqlSessionFactory = sqlSessionFactoryBuilder.build(mybatisInputStream);
 	}
+	
 	@Override
 <<<<<<< Upstream, based on origin/Ateam/기현
 	public int insert(Orders order) throws Exception {
@@ -37,16 +38,26 @@ public class OrdersDaoImpl implements OrdersDao {
 		return 1; 
 >>>>>>> 02c44db orders insert 완성
 	}
+	
 	@Override
 	public List<Orders> findAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		OrdersMapper ordersMapper = sqlSession.getMapper(OrdersMapper.class);
+		sqlSession.close();
+		return ordersMapper.selectAll();
 	}
+	
 	@Override
-	public List<Orders> findOrderByOrderItems() throws Exception {
-		// TODO Auto-generated method stub
+	public List<Orders> findByOrderNo(int orderNo) throws Exception{
 		return null;
 	}
+	
+	
+	@Override
+	public List<Orders> findOrderByOrderItems(int customerNo, int orderNo) throws Exception {
+		return null;
+	}
+	
 	@Override
 	public int deleteOrder(int orderNo) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
