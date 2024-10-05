cbca219bde67a0348f403856f9c60389dd8a9ec5
imjeong <imjeong@jeong>
2024-10-05 22:55:51 +0900

    orders select 완성 , service 구현 완료

--- pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
+++ pineappleProject/src/main/java/com/itwill/shop/mapper/OrdersMapper.java
@@ -13,11 +13,8 @@ public interface OrdersMapper {
 	//주문 전체 리스트(전체) 어드민
 	List<Orders> selectAll() throws Exception;
 	
-	//주문 전체 리스트(특정 사용자)
+	//주문+주문리스트 전체 리스트(특정 사용자)
 	List<Orders> selectByOrderNo(int orderNo) throws Exception;
-	
-	//주문+주문 아이템(특정사용자, 특정 주문)
-	List<Orders> selectOrderByOrderItems(int customerNo, int orderNo) throws Exception;
 	
 	// 주문 1건 삭제(특정 주문)
 	int delete(int orderNo) throws Exception;
--- pineappleProject/src/main/java/com/itwill/shop/orders/OrdersService.java
+++ pineappleProject/src/main/java/com/itwill/shop/orders/OrdersService.java
@@ -1,5 +1,31 @@
 package com.itwill.shop.orders;
 
-public class OrdersService {
+import java.util.List;
 
+public class OrdersService {
+	private OrdersDao ordersDao;
+	
+	public OrdersService() throws Exception {
+		ordersDao = new OrdersDaoImpl();
+	}
+	
+	/*주문 등록*/
+	public int insertOrder(Orders order) throws Exception{
+		return ordersDao.insertOrder(order);
+	}
+	
+	/* [어드민] 주문 전체 리스트(전체) */
+	public List<Orders> finfAll() throws Exception{
+		return ordersDao.findAll();
+	}
+	
+	/*주문 + 주문 아이템 전체 리스트(특정 사용자)*/
+	public List<Orders> findByOrderNo(int orderNo) throws Exception{
+		return ordersDao.findByOrderNo(orderNo);
+	}
+	
+	/*주문 삭제*/
+	public int delete(int orderNo) throws Exception{
+		return ordersDao.deleteOrder(orderNo);
+	}
 }
