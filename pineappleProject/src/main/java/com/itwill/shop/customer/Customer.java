6586110b8c5bffede93596d71967b568ebed7866
lee min young <dlalsdyd0506@naver.com>
2024-10-06 18:06:11 +0900

    Merge branch 'Bteam/customer' of https://github.com/2024-07-JAVA-DEVELOPER-155/web-project-team1-pineapple.git into Bteam/customer

--- pineappleProject/src/main/java/com/itwill/shop/customer/Customer.java
+++ pineappleProject/src/main/java/com/itwill/shop/customer/Customer.java
@@ -11,12 +11,15 @@ import com.itwill.shop.review.Review;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 
 public class Customer {
 	/********** PK ************/
--- pineappleProject/src/main/java/com/itwill/shop/customer/CustomerTest.java
+++ pineappleProject/src/main/java/com/itwill/shop/customer/CustomerTest.java
@@ -38,46 +38,48 @@ public class CustomerTest {
 //		System.out.println(customerService.findCustomerNo(2));
 		
 		/*********** select all *******************/
-		Customer customer =  customerService.findCustomerNoListAll(4);
-		System.out.println(customerService.findCustomerNoListAll(1));
-			System.out.println(customer.getCustomerNo());
-			System.out.println(customer.getCustomerId());
-			System.out.println(customer.getCustomerPassword());
-			System.out.println(customer.getCustomerEmail());
-			System.out.println(customer.getCustomerAddress());
-			System.out.println(customer.getCustomerGender());
-			System.out.println(customer.getCustomerNickname());
-			System.out.println(customer.getCustomerPhone());
-			System.out.println(customer.getCustomerDob());
-			System.out.println(customer.getCustomerName());
-			System.out.println(customer.getCustomerJoindate());
+		Customer customer = customerService.findCustomerNoListAll(2);
+		System.out.println(customer.getCustomerNo());
+		System.out.println(customer.getCustomerId());
+		System.out.println(customer.getCustomerPassword());
+		System.out.println(customer.getCustomerEmail());
+		System.out.println(customer.getCustomerAddress());
+		System.out.println(customer.getCustomerGender());
+		System.out.println(customer.getCustomerNickname());
+		System.out.println(customer.getCustomerPhone());
+		System.out.println(customer.getCustomerDob());
+		System.out.println(customer.getCustomerName());
+		System.out.println(customer.getCustomerJoindate());
+		
 		List<CustomerCoupons> customerCouponList = customer.getCouponList();
-			for (CustomerCoupons customerCoupons : customerCouponList) {
-				System.out.println("/****************쿠폰 번호*************************/");
-				System.out.println(customerCoupons.getCustomer());
-				System.out.println(customerCoupons.getCustomerCouponsNo()); 
-				System.out.println(customerCoupons.getCustomerCouponsStatus());
-				System.out.println(customerCoupons.getCustomerCouponsEnddate());
-				System.out.println("/****************쿠폰 정보*************************/");
-				System.out.println(customerCoupons.getCoupon().getCouponId()); 
-				System.out.println(customerCoupons.getCoupon().getCouponDiscount()); 
-				System.out.println(customerCoupons.getCoupon().getCouponName()); 
-				System.out.println(customerCoupons.getCoupon().getCouponDesc()); 
-			
+		if (customerCouponList != null) {
+		    for (CustomerCoupons customerCoupons : customerCouponList) {
+		        System.out.println("/****************쿠폰 번호*************************/");
+		        System.out.println(customerCoupons.getCustomerCouponsNo());
+		        System.out.println(customerCoupons.getCustomerCouponsStatus());
+		        System.out.println(customerCoupons.getCustomerCouponsEnddate());
+		        System.out.println("/****************쿠폰 정보*************************/");
+		        System.out.println(customerCoupons.getCoupon().getCouponId());
+		        System.out.println(customerCoupons.getCoupon().getCouponDiscount());
+		        System.out.println(customerCoupons.getCoupon().getCouponName());
+		        System.out.println(customerCoupons.getCoupon().getCouponDesc());
+		    }
 		}
-		List<Review> customerReviewList = customer.getReviewList();		
-			for (Review reviewList : customerReviewList) {
-				System.out.println("**************** 고객 리뷰 정보*********************");
-				System.out.println(reviewList.getReviewNo());
-				System.out.println(reviewList.getReviewTitle());
-				System.out.println(reviewList.getReviewContent());
-				System.out.println(reviewList.getReviewImage());
-				System.out.println(reviewList.getReviewRating());
-				
+
+		List<Review> customerReviewList = customer.getReviewList();
+		if (customerReviewList != null) {
+		    for (Review reviewList : customerReviewList) {
+		        System.out.println("**************** 고객 리뷰 정보*********************");
+		        System.out.println(reviewList.getReviewNo());
+		        System.out.println(reviewList.getReviewTitle());
+		        System.out.println(reviewList.getReviewContent());
+		        System.out.println(reviewList.getReviewImage());
+		        System.out.println(reviewList.getReviewRating());
+		    }
+		}
 			
-			
-			}
-			
+		System.out.println("쿠폰 리스트 크기: " + customer.getCouponList().size());
+		System.out.println("리뷰 리스트 크기: " + customer.getReviewList().size());
 			
 		
 		
